50d8fa2e34a1ae5ddd140acd649274e9acb6922f
Darius <darius.vallejo@gmail.com>
2020-06-18 08:15:12 -0700

    Adjusts endpoint config logic, removes cc obfuscation, includes lodging on bookings, comments html

--- angular/src/app/data/review.model.ts
+++ angular/src/app/data/review.model.ts
@@ -1,10 +1,12 @@
+import { Lodging } from './lodging.model';
 /**
  * Represents the _Review_ model
  *
  * ```yaml
  * id: string;
  * accountId: string;
- * hotelId: string;
+ * lodgingId: string;
+ * lodging: Lodging;
  * comment: string;
  * dateCreated: Date;
  * rating: number;
@@ -13,7 +15,8 @@
 export interface Review {
   id: string;
   accountId: string;
-  hotelId: string;
+  lodgingId: string;
+  lodging: Lodging;
   comment: string;
   dateCreated: Date;
   rating: number;
